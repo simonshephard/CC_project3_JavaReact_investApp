ed83a697779f52cd536eaf0c60027373416f25b6
Simon Shephard <simonshephard@hotmail.com>
2018-11-02 15:04:12 +0000

    adds template Java and React apps

---
+++ price_data_server/src/main/java/com/codeclan/example/price_data_server/PriceDataServerApplication.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.price_data_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceDataServerApplication.class, args);
+	}
+}
